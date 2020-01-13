8b91e2b9aab4b4aec0f4bf708252e703294e6bf1
tomoki0307 <goosehouse.sekaowa.melody@gmail.com>
2020-01-14 01:36:16 +0900

    Initial Commit

---
+++ Test_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test_Project
+//
+//  Created by 筒井知生 on 2020/01/14.
+//  Copyright © 2020 筒井知生. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
